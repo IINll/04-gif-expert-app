b67a40f0e7cf392bcd13ddecb337f2d697462d73
IINll <mvillanueva.m27@gmail.com>
2023-01-13 14:54:27 -0300

    Termino clase 80

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,48 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 
 export const GifExpertApp = () => {
+
+
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+
+    const onAddCategory = (newCategory) => {
+
+        if(categories.includes(newCategory)) return;        
+
+        setCategories( [ newCategory, ...categories ]);
+
+    }
+    
 
     return (
         <>
         
             <h1>GifExpertApp</h1>
+
+            <AddCategory 
+                // setCategories={setCategories}
+                onNewCategory={(value) => onAddCategory(value)}
+            
+            
+            />
+            
+            
+
+            
+
+            <ol>
+                {
+                    categories.map(category => {
+
+                    return <li key={category}>{category}</li>
+
+                    })
+                }
+            
+
+            </ol>
 
         </>
     )
