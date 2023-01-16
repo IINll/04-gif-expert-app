e59d7b9efb1f8d2de9c411dd9b006d12da5f9254
IINll <mvillanueva.m27@gmail.com>
2023-01-15 21:39:03 -0300

    Sección 6 terminada

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react"
-import { AddCategory } from "./components/AddCategory";
+import { AddCategory, GifGrid } from "./components";
 
 
 export const GifExpertApp = () => {
 
 
-    const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+    const [categories, setCategories] = useState(['One Punch']);
 
     const onAddCategory = (newCategory) => {
 
         if(categories.includes(newCategory)) return;        
 
         setCategories( [ newCategory, ...categories ]);
+        
 
     }
     
@@ -22,28 +23,19 @@ export const GifExpertApp = () => {
             <h1>GifExpertApp</h1>
 
             <AddCategory 
-                // setCategories={setCategories}
                 onNewCategory={(value) => onAddCategory(value)}
-            
-            
             />
             
             
-
+            {
+                categories.map(category => (
+                    <GifGrid 
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
             
-
-            <ol>
-                {
-                    categories.map(category => {
-
-                    return <li key={category}>{category}</li>
-
-                    })
-                }
-            
-
-            </ol>
-
         </>
     )
 
